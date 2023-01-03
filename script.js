ef5dac3146f03a68e87439965fcef9d656b7fb68
Timothy <119418395+cobaltje@users.noreply.github.com>
2023-01-03 23:13:16 +0100

    Little update

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// Define the variables
+let secretNumber = calculateSecretNumber();
+let score = 20;
+let highscore;
+let previousGuesses = [];
+let guess;
+
+function calculateSecretNumber() {
+  return Math.trunc(Math.random() * 20) + 1;
+}
+
+// Main game logic
+document.querySelector(".check").addEventListener("click", function () {
+  guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+  if (guess === 1) {
+    console.log("not a number");
+  }
+});
